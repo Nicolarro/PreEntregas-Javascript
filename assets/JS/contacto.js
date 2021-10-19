f6e0676eef1b3fd10203b05ef1ff1a7dba9fb09c
Nicolas Larrosa <nicolarro20@gmail.com>
2021-10-18 23:41:15 -0300

    contacto corregido

--- assets/JS/contacto.js
+++ assets/JS/contacto.js
@@ -1,10 +1,19 @@
 
 /* FUNCION QUE TOME LOS DATOS DE CONTACTO DEL USUARIO  Y LOS GUARDA EN BASE DE DATOS*/
 
+class Cliente{
+    constructor(nombre,apellido,email,tipoEvento,comentarios){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.tipoEvento = tipoEvento;
+        this.comentarios = comentarios;
+    }
+}
 
 const agregarCliente =  () =>{
 
-    const cliente  = new Cliente ({
+    const cliente = new Cliente ({
 
     nombre : document.getElementById("nombre").value,
     apellido : document.getElementById("apellido").value,
@@ -12,11 +21,7 @@ const agregarCliente =  () =>{
     tipoEvento : document.getElementById("tipoEvento").value,
     comentarios : document.getElementById("comentarios").value,
 
-})
-    let baseCliente = localStorage.setItem("cliente",agregarCliente())
+    })
+
+    let baseCliente = localStorage.setItem("cliente",agregarCliente());
 }
-    
-
-
-
-console.log(agregarCliente());
